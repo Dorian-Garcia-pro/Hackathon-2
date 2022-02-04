fea8e9c82552486ba8c28d79dbd4161e8d3c00ca
Yannick COUSIN <94236356+yannick-cousin@users.noreply.github.com>
2022-02-04 09:19:17 +0100

    Update

--- src/components/Assurances.js
+++ src/components/Assurances.js
@@ -1,48 +1,57 @@
-import React from "react";
-import "./Assurances.css";
-import assur from "../assets/assur.webp";
+import React from 'react';
+import './Assurances.css';
+import assur from '../assets/assur.webp';
 
-function Assurances() {
-  return (
-    <div className="AssurWrapper">
-      <h2 id="Assur">Assurance</h2>
+const Assurances = (props) => {
+	const updateAssurances = (choice) => {
+		props.setAssurance(1);
+	};
 
-      <div className="Assur">
-        <div className="AssurDroite">
-          <img src={assur} />
-        </div>
-        <div className="AssurGauche">
-          <h2>La tranquillité d’esprit n’a pas de prix</h2>
-          <p>
-            Avec l’assurance voyage Cover Genius, oubliez tous vos soucis et
-            profitez d’un voyage sûr et amusant.
-          </p>
+	return (
+		<div className="AssurWrapper">
+			<h2 id="Assur">Assurance</h2>
 
-          <p>
-            ✓ Prise en charge le remboursement de votre voyage si vous
-            contractez le Covid-19 avant votre départ.
-          </p>
-          <p>
-            ✓ Prise en charge de vos frais médicaux en cas d'accident ou maladie
-            durant votre voyage, jusqu'à 1millions de dollar y compris le
-            Covid-19
-          </p>
-          <p>
-            ✓ Si vous contractez le Covid-19 durant votre voyage et que votre
-            état de santé nécessite un rapatriement sanitaire, AXA organise
-            votre retour en France
-          </p>
-          <div className="AssurAjouter">
-            <div className="AssurPrixParPers">
-              <p className="petitGris">Par personne</p>
-              <p className="gras">99,99€</p>
-            </div>
-            <div className="AssurBtnAjouter">Ajouter pour Tous</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+			<div className="Assur">
+				<div className="AssurDroite">
+					<img src={assur} />
+				</div>
+				<div className="AssurGauche">
+					<h2>La tranquillité d’esprit n’a pas de prix</h2>
+					<p>
+						Avec l’assurance voyage Cover Genius, oubliez tous vos soucis et
+						profitez d’un voyage sûr et amusant.
+					</p>
+
+					<p>
+						✓ Prise en charge le remboursement de votre voyage si vous
+						contractez le Covid-19 avant votre départ.
+					</p>
+					<p>
+						✓ Prise en charge de vos frais médicaux en cas d'accident ou maladie
+						durant votre voyage, jusqu'à 1millions de dollar y compris le
+						Covid-19
+					</p>
+					<p>
+						✓ Si vous contractez le Covid-19 durant votre voyage et que votre
+						état de santé nécessite un rapatriement sanitaire, AXA organise
+						votre retour en France
+					</p>
+					<div className="AssurAjouter">
+						<div className="AssurPrixParPers">
+							<p className="petitGris">Par personne</p>
+							<p className="gras">99,99€</p>
+						</div>
+						<div
+							className="AssurBtnAjouter"
+							onClick={() => updateAssurances(1)}
+						>
+							Ajouter pour Tous
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Assurances;
