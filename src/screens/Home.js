2ea1f7274386ed5f788df8f622d0b48c44c4d693
Dorian-Garcia-pro <garcia.dorian.pro@gmail.com>
2022-02-04 14:36:08 +0100

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -81,12 +81,6 @@ const Home = ({ avatar, setAvatar }) => {
 
   return (
     <div>
-      {console.log(searchActivite)};
-      {/* {console.log("poulet", searchSelectTerm)} */}
-      {/* <div className="topBanner">
-        <h1>C'est quand qu'on arrive ?</h1>
-        {/*     <div className="avatarConnexion">Se connecter</div> */}
-      {/* </div> */}
       <div className="wrapperContent centrer">
         <div className="searchBar">
           <div className="searchSection searchOu ">
