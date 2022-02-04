4723e0e2b8b66390458bfb35cfe7a4ea1434d2e7
Mickaseg <mickael_seguin@hotmail.fr>
2022-02-04 07:21:02 +0100

    added carrousel

--- src/screens/Historique.js
+++ src/screens/Historique.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "../styles/Historique.css";
 import axios from "axios";
 import { useState } from "react";
@@ -42,51 +41,6 @@ const Historique = () => {
       <button onClick={postReservation}>Post reservation</button>
     </div>
   );
-=======
-import '../styles/Historique.css';
-import axios from 'axios';
-import { useState } from 'react';
-
-const Historique = () => {
-	const [date, setDate] = useState('');
-	const [cout, setCout] = useState(0);
-	const [destination, setDestination] = useState('');
-
-	const handleChangeDate = (e) => {
-		let value = e.target.value;
-		setDate(value);
-	};
-
-	const handleChangeCout = (e) => {
-		let value = e.target.value;
-		setCout(value);
-	};
-
-	const handleChangeDestination = (e) => {
-		let value = e.target.value;
-		setDestination(value);
-	};
-
-	const postReservation = () => {
-		axios.post('http://localhost:3030/historiques/historique', {
-			date: date,
-			destination: destination,
-			cout: cout,
-		});
-	};
-
-	return (
-		<div>
-			<input placeholder="date" onChange={handleChangeDate}></input>
-			<input placeholder="cout" onChange={handleChangeCout}></input>
-			<input
-				placeholder="destination"
-				onChange={handleChangeDestination}
-			></input>
-			<button onClick={postReservation}>Post reservation</button>
-		</div>
-	);
->>>>>>> dev
 };
 
 export default Historique;
